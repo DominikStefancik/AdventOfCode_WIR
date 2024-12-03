3fcfe0689e7f3ec416377483ec4fb6fefe778082
Dominik Stefancik <dstefancik@gmail.com>
2024-12-03 20:54:07 +0100

    Implement the second part of the solution for the day 1

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -2,15 +2,17 @@ use std::fs;
 use std::io::Error;
 
 fn main() -> Result<(), Error> {
-    let (vector1, vector2) = parse_input().expect("Error parsing the input file");
+    let (mut vector1, mut vector2) = parse_input().expect("Error parsing the input file");
 
     if vector1.len() != vector2.len() {
         panic!("Total number of left numbers is not equal to the total number of right numbers")
     }
 
-    let sum_of_differences = get_sum_of_differences(vector1, vector2);
+    let sum_of_differences = get_sum_of_differences(&mut vector1, &mut vector2);
+    let similarity_score = get_similarity_score(vector1, vector2);
 
     println!("The sum of differences is: {sum_of_differences}");
+    println!("The similarity score is: {similarity_score}");
 
     Ok(())
 }
@@ -45,7 +47,7 @@ fn parse_input() -> Result<(Vec<u32>, Vec<u32>), Error> {
     Ok((vector1, vector2))
 }
 
-fn get_sum_of_differences(mut vector1: Vec<u32>, mut vector2: Vec<u32>) -> u32 {
+fn get_sum_of_differences(vector1: &mut Vec<u32>, vector2: &mut Vec<u32>) -> u32 {
     vector1.sort();
     vector2.sort();
 
@@ -54,4 +56,18 @@ fn get_sum_of_differences(mut vector1: Vec<u32>, mut vector2: Vec<u32>) -> u32 {
         .zip(vector2.iter())
         .map(|pair| pair.0.abs_diff(*pair.1))
         .sum()
+}
+
+fn get_similarity_score(vector1: Vec<u32>, vector2: Vec<u32>) -> u32 {
+    let mut similarities: Vec<u32> = Vec::new();
+
+    for left_number in vector1.iter() {
+        let count = vector2
+            .iter()
+            .filter(|right_number| left_number == *right_number)
+            .count() as u32;
+        similarities.push(*left_number * count);
+    }
+
+    similarities.iter().sum()
 }
